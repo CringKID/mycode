ac2c3b579f17b4e34fe900135805f6b47428e068
CringKID <819738341@qq.com>
2022-01-13 20:46:51 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+using ll = long long;
+using Pii = pair <int, int>;
+using Pll = pair <ll, ll>;
+const int kMaxN = 1e5 + 5, kMod = 1e9 + 7, kInf = 1e9;
+
+int main () {
+  ios :: sync_with_stdio (false);
+  cin.tie (0), cout.tie (0);
+  int a, b;
+  cin >> a >> b;
+  cout << a + b;
+  return 0;
+}
